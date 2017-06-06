24a838db70059ab576e27c2aa3cd891ec9ffb170
Daniel Halliday <daniel.j.halliday@gmail.com>
2017-06-06 12:43:32 +0100

    initial commit(ignore previous)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
